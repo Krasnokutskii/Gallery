9381d82183a1f60d26a52d6b4fbd505310f2eeb7
Iaroslav Krasnokutskii <Yroslavk1995@gmail.com>
2021-11-29 16:20:38 +0300

    Removed some comments and empty file was deleted

--- Gallery/DetailPaggedViewController.swift
+++ Gallery/DetailPaggedViewController.swift
@@ -27,9 +27,6 @@ class DetailPaggedViewController: UIViewController, UICollectionViewDelegate, UI
         collectionView.delegate = self
         collectionView.dataSource = self
         addBackgroundGradient()
-        //added some coments
-        
-        //some new code
         
     }
     
